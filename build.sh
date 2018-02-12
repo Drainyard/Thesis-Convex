c34323a1a89eb2fe4924fdd7c37d654bd5035b0b
Niels Bross <nielsbross@gmail.com>
2018-02-12 11:40:05 +0100

    made portable version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ WIGNORE="-Wno-zero-as-null-pointer-constant -Wno-old-style-cast"
 
 pushd build
 
-clang -Weverything $WIGNORE -g --std=c++14 ../src/main.cpp -isystem ../libs/glad/include -L/usr/local/lib -L../libs/glad -L -ldl -lm -lGL -lglfw -lglad -o main
+clang -Weverything $WIGNORE -g --std=c++14 ../src/main.cpp -isystem ../libs/glad/include -L/usr/local/lib -L../libs/glad -L../libs -ldl -lm -lGL -lglfw -lglad -o main -Wl,-rpath,\$ORIGIN/../build
 
 popd
 
