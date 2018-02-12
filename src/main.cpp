d414c40f0630fa274095b89a653f90b27c4afcb1
Niels Bross <nielsbross@gmail.com>
2018-02-12 12:27:21 +0100

    first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,103 @@
 static void PrintError()
 {
     if(auto err = glGetError() != GL_NO_ERROR)
-        printf("%d\n", glGetError());
+        printf("%d\n", err);
+}
+
+static char* LoadShaderFromFile(const char* Path)
+{
+    GLchar* Source = {};
+    
+    FILE* File = fopen(Path, "r");
+    if(File)
+    {
+        fseek(File, 0, SEEK_END);
+        uint32_t Size = (uint32_t)ftell(File);
+        fseek(File, 0, SEEK_SET);
+        
+        Source = (GLchar*)malloc(Size + 1 * sizeof(GLchar));
+        
+        fread(Source, Size, 1, File);
+        Source[Size] = '\0';
+        
+        fclose(File);
+    }
+    else
+    {
+        fprintf(stderr, "Could not read file %s. File does not exist.\n",Path);
+    }
+    
+    return Source;
+}
+
+static 
+
+static GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path)
+{
+    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+    
+    auto VertexShaderCode = LoadShaderFromFile(vertex_file_path);
+    auto FragmentShaderCode = LoadShaderFromFile(fragment_file_path);
+    if(!VertexShaderCode)
+    {
+        fprintf(stderr, "Could not load vertex shader: %s\n", vertex_file_path);
+    }
+    if(!FragmentShaderCode)
+    {
+        fprintf(stderr, "Could not load fragment shader: %s\n", fragment_file_path);
+    }
+    
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+    
+    glShaderSource(VertexShaderID, 1, &VertexShaderCode, NULL);
+    glCompileShader(VertexShaderID);
+    
+    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if(InfoLogLength > 0)
+    {
+        char* Buffer = (char*)malloc((InfoLogLength + 1) * sizeof(char));
+        glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &Buffer[0]);
+        printf("%s\n", &Buffer[0]);
+    }
+    
+    
+    glShaderSource(FragmentShaderID, 1, &FragmentShaderCode, NULL);
+    glCompileShader(FragmentShaderID);
+    
+    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if(InfoLogLength > 0)
+    {
+        char* Buffer = (char*)malloc((InfoLogLength + 1) * sizeof(char));
+        glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &Buffer[0]);
+        printf("Bob: %s\n", &Buffer[0]);
+    }
+    
+    printf("Linking program\n");
+    GLuint ProgramID = glCreateProgram();
+    glAttachShader(ProgramID, VertexShaderID);
+    glAttachShader(ProgramID, FragmentShaderID);
+    glLinkProgram(ProgramID);
+    
+    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if(InfoLogLength > 0)
+    {
+        char* Buffer = (char*)malloc((InfoLogLength + 1) * sizeof(char));
+        glGetShaderInfoLog(ProgramID, InfoLogLength, NULL, &Buffer[0]);
+        printf("Bob: %s\n", &Buffer[0]);
+    }
+    
+    glDetachShader(ProgramID, VertexShaderID);
+    glDetachShader(ProgramID, FragmentShaderID);
+    
+    glDeleteShader(VertexShaderID);
+    glDeleteShader(FragmentShaderID);
+    
+    return ProgramID;
 }
 
 int main()
@@ -33,6 +129,7 @@ int main()
         glfwTerminate();
         return -1;
     }
+    
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     
@@ -40,12 +137,10 @@ int main()
     printf("Shading language supported: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
     printf("Glad Version: %d.%d\n", GLVersion.major, GLVersion.minor);
     
-    // Ensure we can capture the escape key being pressed below
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    
-    
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
+    
+    printf("VAO: %d\n", VertexArrayID);
     glBindVertexArray(VertexArrayID);
     
     static const GLfloat g_vertex_buffer_data[] = {
@@ -56,26 +151,29 @@ int main()
     
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
-    PrintError();
+    
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    PrintError();
+    
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-    PrintError();
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    GLuint ProgramID = LoadShaders("shaders/basic.vert", "shaders/basic.frag");
+    
     // Check if the ESC key was pressed or the window was closed
     while(glfwGetKey(window, Key_Escape ) != GLFW_PRESS &&
           glfwWindowShouldClose(window) == 0 )
     {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnableVertexAttribArray(0);
-        PrintError();
+        glUseProgram(ProgramID);
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-        PrintError();
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-        PrintError();
         
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        PrintError();
         glDisableVertexAttribArray(0);
-        PrintError();
         
         // Swap buffers
         glfwSwapBuffers(window);
